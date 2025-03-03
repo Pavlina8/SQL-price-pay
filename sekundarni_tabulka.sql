137edf3f67b9c4d6e748e60a71f90858b33a5b1e
Pavlina8 <Pavlinaaa88@gmail.com>
2025-03-03 18:42:33 +0100

    Add files via upload

---
+++ sekundarni_tabulka.sql
@@ -0,0 +1,22 @@
+CREATE table t_pavlina_kuncova_project_SQL_secondary_final AS     --a t_{jmeno}_{prijmeni}_project_SQL_secondary_final 
+select  	e.country,
+				e.year,
+	 			c.continent,
+	 			e.gdp as HDP,
+	 			e.gini as GINI,
+	 			e.population 
+from economies e 
+join countries c on c.country = e.country
+where c.continent like 'Europe'
+and year > 1999 and e.year < 2022
+order by country ;
+
+select 	tst.year,
+		tst.country,
+		tst.hdp, 
+		tpt.prumerna_mzda, 
+		tpt.name_food,
+		tpt.avg_price_food 
+from t_secondary_table tst
+join t_primarni_tabulka tpt on tpt.year_price =tst.year
+where country like '%Cze%';
